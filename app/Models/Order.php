3a786d104d5966ce6db022e97e17f14fa44fe267
Diu <diuht@minori.com.vn>
2021-04-29 15:08:12 +0700

    Checkout & payment Diu

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,5 +9,5 @@ class Order extends Model
 {
     use HasFactory;
     protected $table = "orders";
-    protected $fillable = ['name', 'phone', 'address', 'order_time', 'note'];
+    protected $fillable = ['name', 'phone', 'address', 'order_time', 'note', 'user_id'];
 }
