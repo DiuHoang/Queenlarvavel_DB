ff0a97776790c95d4ad899cf52d3622b67d5d484
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-04 23:08:12 +0700

    added

--- app/Http/Controllers/admin_order_controller.php
+++ app/Http/Controllers/admin_order_controller.php
@@ -15,7 +15,7 @@ class admin_order_controller extends Controller
     public function get_order_by_vendor($vendor_id){
         $order_list = DB::select("select orders.name, orders.phone, orders.order_time, 
         orders.note, orders.status, orders.orderlist_id, orders.user as id from orders, order_list 
-        where order_list.user_id = orders.user and order_list.vendor_id = $vendor_id group by orders.user");
+        where order_list.user_id = orders.user and order_list.vendor_id = $vendor_id group by orders.name");
         return json_encode($order_list);
         $arr =[];
         for($i =0; $i < count($order_list ); $i++){
