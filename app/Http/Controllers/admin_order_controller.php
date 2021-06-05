42567b9ff51e66cc640aa13337504d116205a56b
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-05 12:05:21 +0700

    Quan

--- app/Http/Controllers/admin_order_controller.php
+++ app/Http/Controllers/admin_order_controller.php
@@ -100,18 +100,18 @@ class admin_order_controller extends Controller
             ->join('order_list', 'order_list.product_id', '=', 'products.id')
             ->where('order_list.user_id', $user_id)
             ->get();
-            $arr=[];
-            array_push($arr,$order);
-            $total =0;
-            for($i =0; $i < count($order); $i++){
-                $totalMoney= DB::table('products')
-                ->join('order_list', 'order_list.product_id', '=', $order[$i]->id)
-                ->where('order_list.user_id', $user_id)
-                ->sum('products.price', '*', 'order_list.quantity');
-                $total +=  $totalMoney;
-            }
-            array_push($arr,$total);
-            return response()->json($arr);
+            // $arr=[];
+            // array_push($arr,$order);
+            // $total =0;
+            // for($i =0; $i < count($order); $i++){
+            //     $totalMoney= DB::table('products')
+            //     ->join('order_list', 'order_list.product_id', '=', $order[$i]->id)
+            //     ->where('order_list.user_id', $user_id)
+            //     ->sum('products.price', '*', 'order_list.quantity');
+            //     $total +=  $totalMoney;
+            // }
+            // array_push($arr,$total);
+            return response()->json($order);
     }
 
     public function user_order($id){
