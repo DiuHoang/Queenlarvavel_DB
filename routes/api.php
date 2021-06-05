abfaf894331c22ea186f2469dac2d8ea9cb9ac84
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-05 23:13:04 +0700

    added

--- routes/api.php
+++ routes/api.php
@@ -169,4 +169,4 @@ Route::get('/test', [CheckoutController::class, 'getProductVendo']);
 
 //VENDOR
 Route::patch('/vendor_update_profile/{id}', [vendor_update_profile_controller::class, 'update_profile']);
-Route::get('total_cart/{vendor_id}', [vendor_dashboard_controller::class, 'user_order']);
+Route::get('total_card/{vendor_id}', [vendor_dashboard_controller::class, 'total_card']);
