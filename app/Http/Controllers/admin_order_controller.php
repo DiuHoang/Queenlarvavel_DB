41c82cb02d7628e5f6d4aac8280e2d6b35822d70
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-05 10:51:55 +0700

    added

--- app/Http/Controllers/admin_order_controller.php
+++ app/Http/Controllers/admin_order_controller.php
@@ -104,7 +104,7 @@ class admin_order_controller extends Controller
             products.picture, products.discount.products.quantity, products.description, 
             sum(products.price * order_list.quantity) as total money from products,
             order_list where products.id = order_list.product_id and order_list.user_id = $user_id 
-            group by products.id")
+            group by products.id");
             return response()->json($order);
     }
 
