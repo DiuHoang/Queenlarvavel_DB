dd1a06f50192b8a84dc5faf2c0aeca4239ff2c9c
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-05 23:23:02 +0700

    added

--- app/Http/Controllers/vendor_dashboard_controller.php
+++ app/Http/Controllers/vendor_dashboard_controller.php
@@ -16,17 +16,17 @@ class vendor_dashboard_controller extends Controller
         $totalOrder = DB::table('orders')
         ->join('order_list', 'order_list.id', '=', 'orders.orderlist_id')
         ->where('order_list.vendor_id', '=', $vendor_id)
-        ->where('orderlist.user_id', '=', 'orders.user');
+        ->where('orderlist.user_id', '=', 'orders.user')
         ->count('orders.user');
 
 
-        $totalRevenue = 0;
-        // DB::table('products')
-        // ->join('order_list', 'order_list.product_id', '=', 'products.id')
-        // ->join('orders', 'order_list.user_id', '=', 'orders.user')
-        // ->where('order_list.vendor_id', '=', $vendor_id)
-        // ->where('orders.status', '=', 'ĐH thành công')
-        // ->sum('products.price', '*', 'order_list.quantity');
+        $totalRevenue =
+        DB::table('products')
+        ->join('order_list', 'order_list.product_id', '=', 'products.id')
+        ->join('orders', 'order_list.user_id', '=', 'orders.user')
+        ->where('order_list.vendor_id', '=', $vendor_id)
+        ->where('orders.status', '=', 'ĐH thành công')
+        ->sum('products.price', '*', 'order_list.quantity');
 
 
         $total_card =["totalUser"=>$totalUsers, "totalOrder"=>$totalOrder, "totalRevenue"=>$totalRevenue];
