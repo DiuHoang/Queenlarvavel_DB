4e612f82e323828fcb37c738f6f3d1b45d48ee9f
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-06 05:27:44 +0700

    Added

--- app/Http/Controllers/vendor_dashboard_controller.php
+++ app/Http/Controllers/vendor_dashboard_controller.php
@@ -23,7 +23,7 @@ class vendor_dashboard_controller extends Controller
         ->join('orders', 'orders.orderlist_id', '=', 'order_list.id')
         ->where('orders.status', '=', 'ĐH thành công')
         ->where('order_list.vendor_id', '=', $vendor_id)
-        ->sum(2 * 3);
+        ->sum('products.price','+', 'order_list.quantity');
 
 
         $total_card =["totalUser"=>$totalUsers, "totalOrder"=>$totalOrder, "totalRevenue"=>$totalRevenue];
