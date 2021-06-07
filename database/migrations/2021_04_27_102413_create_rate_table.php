6e6629cd98a494a4d31041aaf91e8c673205b44c
DiuHoang <diu.hoang21@student.passerellesnumeriques.org>
2021-06-07 21:52:25 +0700

    Added

--- database/migrations/2021_04_27_102413_create_rate_table.php
+++ database/migrations/2021_04_27_102413_create_rate_table.php
@@ -16,7 +16,7 @@ class CreateRateTable extends Migration
         
         Schema::create('rate', function (Blueprint $table) {
             $table->id();
-            $table->string('quantity');
+            $table->integer('quantity');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('product_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
